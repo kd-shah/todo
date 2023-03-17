7a87f876baef2a13fe977f6fdf36415f183e9440
kd-shah <kunal.190410107121@gmail.com>
2023-03-17 10:40:06 +0530

    first commit

---
+++ src/Routes/LoginForm/index.js
@@ -0,0 +1,35 @@
+import "./style.scss";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginForm() {
+  const navigate = useNavigate();
+
+  function onSubmitEventHandler() {
+    navigate("/todo");
+  }
+
+  return (
+    <>
+      <form onSubmit={onSubmitEventHandler} className="form">
+        <div className="fields">
+          <label>
+            Name
+            <input className="input" required />
+          </label>
+        </div>
+        <div className="fields">
+          <label>
+            E-Mail
+            <input type={"email"} className="input" required />
+          </label>
+        </div>
+
+        <div className="fields">
+          <button type="submit" className="mainBt">
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
