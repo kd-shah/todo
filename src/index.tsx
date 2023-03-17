b2894a5430952af185c8b20b3b97d44dae460760
kd-shah <kunal.190410107121@gmail.com>
2023-03-17 11:56:03 +0530

    name change

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import {App} from "./App";
 import { BrowserRouter } from "react-router-dom";
 import {reportWebVitals} from "./reportWebVitals";
-//declare reportWebVitals require("./reportWebVitals");
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
